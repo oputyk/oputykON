c049f0294ebfbeffcac7149333c020d80b504352
oputyk <kamilp06@gmail.com>
2017-08-31 15:42:20 +0200

    Category DTO.

---
+++ app/src/main/java/com/example/kamil/otomotonotifier/Category.java
@@ -0,0 +1,53 @@
+package com.example.kamil.otomotonotifier;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+/**
+ * Created by kamil on 29/08/2017.
+ */
+
+public class Category {
+    private String id;
+    private String name;
+    private String nameEn;
+    private List<Category> subCategories;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty(value = "name_en")
+    public String getNameEn() {
+        return nameEn;
+    }
+
+    @JsonProperty(value = "name_en")
+    public void setNameEn(String nameEn) {
+        this.nameEn = nameEn;
+    }
+
+    @JsonProperty(value = "children")
+    public List<Category> getSubCategories() {
+        return subCategories;
+    }
+
+    @JsonProperty(value = "children")
+    public void setSubCategories(List<Category> subCategories) {
+        this.subCategories = subCategories;
+    }
+}
