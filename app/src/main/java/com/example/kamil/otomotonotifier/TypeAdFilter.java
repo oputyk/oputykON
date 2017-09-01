89a5fdcfa1ee030b57ea5577b641608bc416bf45
oputyk <kamilp06@gmail.com>
2017-09-01 14:59:01 +0200

    Filtering added v2.

---
+++ app/src/main/java/com/example/kamil/otomotonotifier/TypeAdFilter.java
@@ -0,0 +1,27 @@
+package com.example.kamil.otomotonotifier;
+
+/**
+ * Created by kamil on 01/09/2017.
+ */
+
+public class TypeAdFilter extends AdFilter {
+    String type;
+
+    @Override
+    boolean isTheAdGoodToAdd(Ad ad) {
+        String adType = ad.getType();
+        if(AdFilterTools.stringsEqual(type, adType)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
